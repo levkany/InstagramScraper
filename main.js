da8c230e9f2ad1aed31ce2bbdda0888e17954850
levkany <levkany.dev@gmail.com>
2020-06-14 19:32:57 +0300

    added - detailed user fetching

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   function getProfiles() {
-
     $.ajax({
       url: 'get_profiles.inc.php',
       type: 'post',
@@ -19,6 +18,14 @@ $(document).ready(function() {
     });
   }
 
+
+  $('#closeProfile').on('click', function() {
+    $('#closeProfile').fadeOut();
+    $('#activeProfile').fadeOut().html('');
+    $('#profiles').fadeIn();
+  });
+
+
   // check whether the previous value equals to the current value after x seconds
   function compareAndFetch() {
     var previousValue = $('#query').val();
@@ -35,5 +42,34 @@ $(document).ready(function() {
 
   $('#query').on('keyup', function() {
     compareAndFetch();
+  });
+
+
+  // when we click any profile image, it will send a request to fetch more detailed information about the user.
+  $('#profiles').on('click', 'a.profile', function() {
+    $(this).parent().fadeOut();
+    $(this).parent().parent().find('#activeProfile').fadeIn();
+
+    // request more user details using ajax
+    $.ajax({
+      url: 'get_profile.inc.php',
+      type: 'post',
+      data: {
+        username: $(this).attr('data-user')
+      },
+
+      success: function(res) {
+        $('#activeProfile').html(res);
+        $('#closeProfile').fadeIn();
+
+        // Init the magnific popup for the dynamic images
+        $('#activeProfile').magnificPopup({
+          delegate: 'a', // child items selector, by clicking on it popup will open
+          type: 'image',
+          gallery: { enabled: true },
+          mainClass: 'mfp-fade'
+        });
+      }
+    });
   });
 });
