363e4dbff04f5663a0b913341cc26c35fee1f05a
levkany <levkany.dev@gmail.com>
2020-06-15 20:51:07 +0300

    seperated inline stylesheet to external file

--- index.php
+++ index.php
@@ -11,75 +11,19 @@
 
     <!-- Main js -->
     <script src='main.js'></script>
+    <link rel="stylesheet" href="style.css">
 
-    <style media="screen">
-      main{
-        max-width: 50vw;
-        margin: auto;
-        text-align: center;
-        padding: 16px;
-      }
-
-      label[for='query'], h2{
-        display: block;
-        color: #003300;
-        padding: 16px;
-        font-family: sans-serif;
-        font-size: 2em;
-      }
-
-      h2 {
-        padding-top: 16px;
-      }
-
-      hr{
-        border: 1px solid lightgray;
-        border-radius: 50%;
-        margin-top: -32px;
-      }
-
-      #profiles, #activeProfile{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        margin-top: 30px;
-      }
-
-      section header{
-        width: 100%;
-      }
-
-      input{
-        border: none;
-        font-family: sans-serif;
-        font-size: 1.5em;
-        border: 1px solid gray;
-        padding: 6px;
-        text-align: center;
-        border-radius: 6px;
-        background: #fffff7;
-      }
-
-      img{
-        max-height: 120px;
-        flex-basis: 10%;
-        margin: 2px;
-        border: 1px solid black;
-      }
-
-      #closeProfile{
-        display: none;
-      }
-    </style>
   </head>
   <body>
     <main>
-      <section id='search'>
-        <label for="query">Search</label>
-        <input type="text" id='query' placeholder="Search for username">
+      <section id='search'><hr/>
+        <div class="content-wrapper">
+          <label for="query">Instagram Query 1.0</label>
+          <input type="text" id='query' placeholder="Search for username">
+        </div>
       </section>
 
-      <h2>Profiles <span id="closeProfile"><a href="#">Go back</a></span></h2><hr/>
+      <h2>Profiles <span id="closeProfile"><a href="#">Go back</a></span></h2>
       <section id='profiles'></section>
 
       <section id='activeProfile' style="display: none;">
