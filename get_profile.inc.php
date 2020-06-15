72f1f5549671fc9b218c7dd37e3555be8bfb50c3
levkany <levkany.dev@gmail.com>
2020-06-15 19:56:32 +0300

    echo markup changes

--- get_profile.inc.php
+++ get_profile.inc.php
@@ -23,11 +23,28 @@
       $profileMedias = $profile->getMedias();
       $mediaMaxDisplay = 10;
 
+      echo '<div class="box">
+              <div class="title"> <h4>'. $profile->getFullName() .'</h4> </div>
+              <div class="content">
+                <div class="bio">Biography '. $profile->getBiography() .'</div>
+                <div class="followers">Followers '. $profile->getFollowedByCount() .'</div>
+                <div class="facebook">Facebook link '. $profile->getConnectedFbPage() .'</div>
+                <div class="medias">Medias '. $profile->getMediaCount() .'</div>
+              </div>
+            </div>
+            ';
+
       $counter = 0;
       foreach($profileMedias as $media){
         if($counter < $mediaMaxDisplay){
           $counter++;
-          echo '<div class="profileImage"><a href="'. $media->getImageHighResolutionUrl() .'"><img src="'. $media->getImageHighResolutionUrl() .'"/></a></div>';
+          echo  '<div class="media">
+                <a href="'. $media->getImageHighResolutionUrl() .'"><img src="'. $media->getImageHighResolutionUrl() .'"/></a>
+                <p>Title '. $media->getCaption() .'</p>
+                <p>Likes '. $media->getLikesCount() .'</p>
+                <p>Comments '. $media->getCommentsCount() .'</p>
+                <p>Subs AVG - '. (($media->getCommentsCount() + $media->getLikesCount()) / $profile->getFollowedByCount()) .'</p>
+                </div>';
         }
 
         // Exit loop if we reached max media display
